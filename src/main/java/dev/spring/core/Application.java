3ad7367007769d9e3549df33d9b1da44bc63a481
combrain <skrtn3410@gmail.com>
2023-11-21 10:54:10 +0900

    AnnotationConfig로 복구

--- src/main/java/dev/spring/core/Application.java
+++ src/main/java/dev/spring/core/Application.java
@@ -1,15 +1,13 @@
 package dev.spring.core;
 
 import dev.spring.core.config.AppConfig;
+import dev.spring.core.service.CourseService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import dev.spring.core.service.CourseService;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new GenericXmlApplicationContext("appConfig.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         CourseService courseService = applicationContext.getBean("courseService", CourseService.class);
         System.out.println(courseService.list());
     }
