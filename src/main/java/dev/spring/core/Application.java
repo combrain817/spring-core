89e555c88d716060b4c84258d4a2bedf11b55fab
combrain <skrtn3410@gmail.com>
2023-11-21 10:38:48 +0900

    GenericXmlApplicationContext로 변경

--- src/main/java/dev/spring/core/Application.java
+++ src/main/java/dev/spring/core/Application.java
@@ -4,12 +4,13 @@ import dev.spring.core.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import dev.spring.core.service.CourseService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        CourseService service = applicationContext.getBean("courseService", CourseService.class);
-        System.out.println(service.list());
-
+        ApplicationContext applicationContext = new GenericXmlApplicationContext("appConfig.xml");
+        CourseService courseService = applicationContext.getBean("courseService", CourseService.class);
+        System.out.println(courseService.list());
     }
 }
